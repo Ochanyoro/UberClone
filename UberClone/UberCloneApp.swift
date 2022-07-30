7d42e2d50d3b045d23cf7b5734ef32d73236d363
Ochanyoro <wadaoo0223@icloud.com>
2022-07-30 12:20:34 +0900

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by 大和田一裕 on 2022/07/30.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
