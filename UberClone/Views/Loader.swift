b561c130da1748cfcdb775e7d42518e8a63178d2
Ochanyoro <wadaoo0223@icloud.com>
2022-07-31 16:11:54 +0900

    [create] SearchView

---
+++ UberClone/Views/Loader.swift
@@ -0,0 +1,42 @@
+//
+//  Loader.swift
+//  UberClone
+//
+//  Created by 大和田一裕 on 2022/07/31.
+//
+
+import SwiftUI
+
+struct Loader : View {
+    
+    @State var show = false
+    
+    var body: some View{
+        
+        GeometryReader{_ in
+            
+            VStack(spacing: 20){
+                
+                Circle()
+                    .trim(from: 0, to: 0.7)
+                    .stroke(Color.red, style: StrokeStyle(lineWidth: 4, lineCap: .round))
+                    .frame(width: 30, height: 30)
+                    .rotationEffect(.init(degrees: self.show ? 360 : 0))
+                    .onAppear {
+                        
+                        withAnimation(Animation.default.speed(0.45).repeatForever(autoreverses: false)){
+                            
+                            self.show.toggle()
+                        }
+                }
+                
+                Text("Please Wait....")
+            }
+            .padding(.vertical, 25)
+            .padding(.horizontal, 40)
+            .background(Color.white)
+            .cornerRadius(12)
+        }
+        .background(Color.black.opacity(0.25).edgesIgnoringSafeArea(.all))
+    }
+}
